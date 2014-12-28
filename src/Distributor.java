578fa8d88a9d15ad65e955b5f89a277e25ec0d2d
mixisbad <pattanapoomy@gmail.com>
2014-12-27 19:05:58 -0800

    update

--- src/Distributor.java
+++ src/Distributor.java
@@ -12,7 +12,11 @@ import java.util.Scanner;
 
 public class Distributor {
 	public static void main(String arg[]) throws IOException {
-		int hosts = 20;
+		distribute(1);
+	}
+	
+	public static void distribute(int hosts) throws IOException
+	{
 		int counter = 0;
 		File file = new File("access.log.3.replay");
 		List<Long> iplist = new ArrayList<Long>();
@@ -113,6 +117,111 @@ public class Distributor {
 					}
 					writer.close();
 				}
+			} catch (UnsupportedEncodingException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+
+		catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void measureSize() throws IOException
+	{
+		int counter = 0;
+		File file = new File("access.log.3.replay");
+		List<Long> iplist = new ArrayList<Long>();
+		HashMap<String,Long> size_from_header = new HashMap<String,Long>();
+
+		/*List<String>[] receivedLog = new List[hosts];
+		for (int i = 0; i < hosts; ++i) {
+			receivedLog[i] = new ArrayList<String>();
+		}*/
+		
+		List<String> receivedLog = new ArrayList<String>();
+
+		try {
+			Scanner sc = new Scanner(file);
+			String line;
+			String addr;
+			int separator;
+			Long decimalAddr;
+			
+			int req_separate;
+			int start_url;
+			int end_url;
+			
+			String based_url = "http://www.cs.hbg.psu.edu";
+			String full_url;
+			String path_url;
+			Long content_length;
+			
+
+			while (sc.hasNextLine()) {
+				line = sc.nextLine();
+								
+				separator = line.indexOf(';');
+				//addr = line.substring(0, separator);
+				//decimalAddr = ipToLong(addr);
+				
+				
+				
+				
+				req_separate = line.indexOf("GET");
+				if(req_separate == -1)
+				{
+					req_separate = line.indexOf("HEAD");
+					if(req_separate == -1)
+					{
+						req_separate = line.indexOf("POST");
+					}
+					if(req_separate == -1)continue;
+						
+					start_url = req_separate+5; 
+				}
+				
+				else
+				{
+					start_url = req_separate+4;
+				}
+				
+				end_url = line.lastIndexOf(" ");
+				
+				System.out.println("[" + line.substring(start_url, end_url) + "]");
+				
+				path_url = line.substring(start_url, end_url);
+				
+				content_length = size_from_header.get(path_url);
+				
+				if(content_length == null)
+				{
+					full_url = based_url + path_url;
+					content_length = getHeaderDetail(full_url);
+					if(content_length == null)continue;
+					else
+					{
+						size_from_header.put(path_url, content_length);
+					}
+				}
+				
+				receivedLog.add(line+";"+content_length);
+				
+			}
+
+			sc.close();
+			
+			try {
+				PrintWriter writer;
+				writer = new PrintWriter("output/measuredHeader.txt","UTF-8");
+
+				for(int j = 0 ; j < receivedLog.size();++j)
+				{
+					writer.println(receivedLog.get(j));
+				}
+				writer.close();				
 			} catch (UnsupportedEncodingException e) {
 				// TODO Auto-generated catch block
 				e.printStackTrace();
